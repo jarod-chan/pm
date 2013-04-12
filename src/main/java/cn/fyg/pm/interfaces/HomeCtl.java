ece823e60fbe03604348e844533dbf80c678cf5b
jhon_chen <jhon.chen@gmail.com>
2013-04-12 10:19:41 +0800

    用户欢迎页面

---
+++ src/main/java/cn/fyg/pm/interfaces/HomeCtl.java
@@ -0,0 +1,15 @@
+package cn.fyg.pm.interfaces;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeCtl {
+	
+	@RequestMapping(value="home",method=RequestMethod.GET)
+	public String toHome(){
+		return "home";
+	}
+
+}
