15a08bb9a0bc2c96135c379d91cdbbed98351120
jhon_chen <jhon.chen@gmail.com>
2013-04-24 14:08:10 +0800

    添加jquery ui日历

---
+++ src/main/java/cn/fyg/pm/domain/constructcont/ConstructContFactory.java
@@ -0,0 +1,19 @@
+package cn.fyg.pm.domain.constructcont;
+
+import java.util.Date;
+import java.util.UUID;
+
+import cn.fyg.pm.domain.user.User;
+
+public class ConstructContFactory {
+	
+	public static ConstructCont create(User creater){
+		ConstructCont constructCont = new ConstructCont();
+		constructCont.setNo(UUID.randomUUID().toString().toUpperCase().substring(0,4));
+		constructCont.setState(ConstructContState.new_);
+		constructCont.setCreater(creater);
+		constructCont.setCreateTime(new Date());
+		return constructCont;
+	}
+
+}
